66f165fbc40ae9bbeff7b6d06aa5c35d663593dd
ItsAdamLY <84637112+ItsAdamLY@users.noreply.github.com>
2022-04-09 01:18:50 +0800

    updated the code, now bug free

--- RandomGuess.cs
+++ RandomGuess.cs
@@ -6,45 +6,84 @@
         {
             Random random = new Random();
             int randomNo = random.Next(1, 20);
-            int inputNo = 0, guess = 5;
+            string input;
+            int guess = 5, inputNo = 0, prevGuess = 0, hint = 3;
 
             while (guess != 0)
             {
                 do
                 {
-                    Console.WriteLine("Enter a number between 1 and 20.");
+                    Console.WriteLine("Enter a number between 1 and 20. You have " + 
+                        hint + " hints left and " + guess + " guesses left.");
 
-                    try
+                    input = Console.ReadLine();
+
+                    while (true)
                     {
-                        inputNo = Convert.ToInt32(Console.ReadLine());
+                        if (input.ToLower() == "hint" && guess < 5)
+                        {
+                            if (prevGuess > randomNo && hint > 0)
+                            {
+                                Console.WriteLine("Your previous number " + prevGuess + " is HIGHER than " +
+                                    "the correct answer.");
+                                --hint;
+                            }
+
+                            else if (prevGuess < randomNo && hint > 0)
+                            {
+                                Console.WriteLine("Your previous number " + prevGuess + " is LOWER than " +
+                                    "the correct answer.");
+                                --hint;
+                            }
+
+                            else if (hint == 0)
+                            {
+                                Console.WriteLine("You are out of hints.");
+                            }
+                        }
+
+                        else if (input.ToLower() == "hint" && guess == 5)
+                            Console.Write("Make a guess first!\n");
+
+                        else if (int.TryParse(input, out inputNo))
+                            break;
+
+                        else
+                            Console.WriteLine("That is not an integer!");
+
+                        Console.WriteLine("\nEnter a number between 1 and 20. You have " +
+                        hint + " hints left and " + guess + " guesses left.");
+
+                        input = Console.ReadLine();
                     }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("This is not an integer!");
-                        continue;
-                    }
+                    
+                    inputNo = Convert.ToInt32(input);
 
                     if (inputNo < 1 || inputNo > 20)
-                        Console.WriteLine("Your input number is out of range, dummy!");
+                    {
+                        Console.WriteLine("Your guess is out of range!");
+                    }
+
 
                 } while (inputNo < 1 || inputNo > 20);
 
                 --guess;
+                prevGuess = inputNo;
 
                 if (inputNo != randomNo && guess != 0)
                 {
-                    Console.WriteLine("Sorry, your guess is incorrect. You have " + guess +
+                    Console.WriteLine("\nSorry, your guess is incorrect. You have " + guess +
                         " guesses left.");
                 }
 
                 else if (inputNo != randomNo && guess == 0)
-                    Console.WriteLine("Sorry, all of your guesses are incorrect. The correct " +
+                    Console.WriteLine("\nSorry, all of your guesses are incorrect. The correct " +
                         "answer is : " + randomNo);
 
 
                 else
                 {
-                    Console.WriteLine("Congratulations! You have guessed the correct number : " + randomNo + "!");
+                    Console.WriteLine("\nCongratulations! You have guessed the correct number : " + randomNo + "!");
                     break;
                 }
 
